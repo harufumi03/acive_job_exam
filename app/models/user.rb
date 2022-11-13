129020dcbc5001485dfa3b29b4340452ee58f936
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-11-13 15:06:28 +0900

    Active Storage実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  has_one_attached :portrait
+  has_one_attached :profile_image
 end
